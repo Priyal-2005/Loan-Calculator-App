dcb4c9d3ac47f69e5af0aea678c1678321dfb89c
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-05-02 20:04:36 +0530

    navbar updated

---
+++ components/NavBar.js
@@ -0,0 +1,10 @@
+export default function NavBar() {
+    return (
+      <nav style={{ padding: '1rem', background: '#eee' }}>
+        <a href="/calculator">Calculator</a> | 
+        <a href="/compare">Compare</a> | 
+        <a href="/export">Export</a> | 
+        <a href="/visualize">Visualize</a>
+      </nav>
+    );
+  }
