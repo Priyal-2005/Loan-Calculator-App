83a6f18ae1b7ff1a902101bf42fda193c9122509
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-05-02 23:25:06 +0530

    updated nvabar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,17 @@
+import Link from 'next/link';
+
 export default function NavBar() {
-    return (
-      <nav style={{ padding: '1rem', background: '#eee' }}>
-        <a href="/calculator">Calculator</a> | 
-        <a href="/compare">Compare</a> | 
-        <a href="/export">Export</a> | 
-        <a href="/visualize">Visualize</a>
-      </nav>
-    );
+  return (
+    <nav>
+      <div className="navbar-container">
+        <div className="navbar-brand">LoanCalc</div>
+        <div className="navbar-links">
+          <Link href="/calculator">Calculator</Link>
+          <Link href="/compare">Compare</Link>
+          <Link href="/export">Export</Link>
+          <Link href="/visualize">Visualize</Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
